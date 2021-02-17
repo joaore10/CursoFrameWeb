b2855ad6d0289729d696a922b8dce3ef70320179
João Vitor de Almeida Ré <joaore1000@gmail.com>
2021-02-17 14:03:14 -0300

    Registrando a Rota do Serviço de Sumário de Pagamentos

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -9,4 +9,7 @@ module.exports = function(server){
    //rotas da API
    const billingCyclesService = require('../api/billingCycle/billingCycleService')
    billingCyclesService.register(router, '/billingCycles')
+
+   const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+   router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
