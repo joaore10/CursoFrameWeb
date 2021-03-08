1d59e3bc1a1ecbeb302d08b1ddb0ca65446fc65d
João Vitor de Almeida Ré <joaore1000@gmail.com>
2021-03-08 14:51:00 -0300

    Estrutura Basica do Login/Cadastro

--- angular1/app/auth/authController.js
+++ angular1/app/auth/authController.js
@@ -7,6 +7,20 @@
 
     function AuthController($location, msgs){
         const vm = this
+
+        vm.loginMode = true
+
+        vm.changeMode = () => vm.loginMode = !vm.loginMode
+
+        vm.login = () => {
+            console.log(` Login.. ${vm.user.email}`)
+        }
+
+        vm.signup = () => {
+            console.log(` Signup.. ${vm.user.email}`)
+        }
+
+
         vm.getUser = () => ({ name: 'Usuário Mock', email: 'mock@cod3r.com.br' })
 
         vm.logout = () => {
