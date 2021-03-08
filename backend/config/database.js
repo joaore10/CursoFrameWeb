05bcb2412c0d26ebcb26250468220e15dbf158a9
João Vitor de Almeida Ré <joaore1000@gmail.com>
2021-03-08 20:31:25 -0300

    Backend configurado para o Heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
 module.exports = mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true })
 
@@ -6,3 +6,4 @@ mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'"
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maximo de '{MAX}'"
 mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}.'"
+
