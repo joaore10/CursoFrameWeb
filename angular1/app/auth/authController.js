f793125eeb6e0ec864749c3e435b02f6a0e2ce1f
João Vitor de Almeida Ré <joaore1000@gmail.com>
2021-03-08 14:16:18 -0300

    Widget do Usuário

---
+++ angular1/app/auth/authController.js
@@ -0,0 +1,15 @@
+
+    angular.module('primeiraApp').controller('AuthCtrl', [
+        '$location',
+        'msgs',
+        AuthController
+    ])
+
+    function AuthController($location, msgs){
+        const vm = this
+        vm.getUser = () => ({ name: 'Usuário Mock', email: 'mock@cod3r.com.br' })
+
+        vm.logout = () => {
+            console.log('logout...')
+        }
+    }
